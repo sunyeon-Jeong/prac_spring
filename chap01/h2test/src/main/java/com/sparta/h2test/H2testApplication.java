a8f08ad697fa1f9daee92f00aaf50c2ec54297a3
sunyeon-Jeong <sunyeon-Jeong>
2023-02-07 21:30:08 +0900

    DATABASE와 SQL 실습

---
+++ chap01/h2test/src/main/java/com/sparta/h2test/H2testApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.h2test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2testApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H2testApplication.class, args);
+    }
+
+}
---
+++ chap01/h2test/src/test/java/com/sparta/h2test/H2testApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.h2test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2testApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
