824ee2e8525ff27db0a1ce2c9a7963d488cafb9a
sunyeon-Jeong <sunyeon-Jeong>
2023-02-21 04:32:31 +0900

    Spring 입문주차 과제_게시글작성 개발(ResponseDto 추가)

--- chap01/hanghaeblog/src/main/java/com/sparta/hanghaeblog/dto/BlogResponseDto.java
+++ chap01/hanghaeblog/src/main/java/com/sparta/hanghaeblog/dto/BlogResponseDto.java
@@ -1,4 +1,28 @@
 package com.sparta.hanghaeblog.dto;
 
+import com.sparta.hanghaeblog.entity.Blog;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class BlogResponseDto {
+    private String title; // 제목
+    private String user; // 사용자
+    private String content; // 작성내용
+    private String password; // 비밀번호
+    private LocalDateTime createdAt; // 생성된 시간
+    private LocalDateTime modifiedAt; // 변경된 시간
+
+    // ResponseDto의 멤버변수 <- Entity blog 클래스 값 getter(모든 수정, 변경이 끝난 값)
+    @Builder
+    public BlogResponseDto(Blog blog) {
+        this.title = blog.getTitle();
+        this.user = blog.getUser();
+        this.content = blog.getContent();
+        this.password = blog.getPassword();
+        this.createdAt = blog.getCreatedAt();
+        this.modifiedAt = blog.getModifiedAt();
+    }
 }
