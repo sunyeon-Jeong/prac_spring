c694aa34197ec664304b7f2e5f12c0ff0dcf5440
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-02-23 12:31:17 +0900

    Spring 숙련주차 실습(Prepare)_프로젝트 생성

---
+++ chap02/myselectshopbeta/src/main/java/com/sparta/myselectshopbeta/MyselectshopbetaApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.myselectshopbeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyselectshopbetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyselectshopbetaApplication.class, args);
+    }
+
+}
