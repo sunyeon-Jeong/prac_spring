6773e04bad481a63e5865ca8bf7164f96c38379b
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-02-26 21:04:04 +0900

    Spring 숙련주차 실습(Auth)_인증/인가 개발 #11 User entity, 등급권한 Enum 개발

---
+++ chap02/myselectshop/src/main/java/com/sparta/myselectshop/entity/User.java
@@ -0,0 +1,38 @@
+package com.sparta.myselectshop.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor // class 기본생성자를 자동으로 추가해줌
+@Entity(name = "users") // 테이블이름 users
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // nullable: null 허용 여부
+    // unique: 중복 허용 여부 (false 일때 중복 허용)
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING) // 권한, 등급을 나타냄(STRING:Enum이름을 저장함)
+    private UserRoleEnum role;
+
+    public User(String username, String password, String email, UserRoleEnum role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+}
---
+++ chap02/myselectshop/src/main/java/com/sparta/myselectshop/entity/UserRoleEnum.java
@@ -0,0 +1,6 @@
+package com.sparta.myselectshop.entity;
+
+public enum UserRoleEnum {
+    USER, // 사용자권한
+    ADMIN // 관리자권한
+}
